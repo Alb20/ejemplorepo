2e4259d816442535416b894b4784686f44cd2b66
Alb20 <alberto_sl@tesch.edu.mx>
2022-10-15 09:47:13 -0500

    Se suben los codigo creados en clase

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ejemplorepo
 subir archivos de las practicas realizadas en clase
+ Subi practicas de la materia de la unidad 1 y 2
---
+++ abreviaciones.js
@@ -0,0 +1,17 @@
+// function suma (a,b){
+//     return a+b;
+// }
+
+// let suma = function ( a,b ){
+//     return a * b;
+// }
+
+// let suma =(a,b) => {
+//     return a +b ;
+// }
+
+let suma = (a ,b) => (a+b)/2;
+
+let promedio = 
+
+console.log (suma(8,9))
